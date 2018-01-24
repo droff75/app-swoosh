1636c5065016dda429be3d9c3b14d3581e385683
David Roff <David.Roff@asynchrony.com>
2018-01-24 13:31:47 -0600

    added iboutlets for league buttons and button logic

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by david.roff on 1/24/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
